f49966f4c22da890429576faaa61a73b7f2b74f4
laurianemollier <mollierlauriane@gmail.com>
2018-01-13 17:42:04 +0100

    first commit

---
+++ src/main/java/io/organis_action/deployment/App.java
@@ -0,0 +1,13 @@
+package io.organis_action.deployment;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
